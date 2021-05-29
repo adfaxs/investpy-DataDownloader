2775a9962ad4493ecbf73cd44aaecfa1aa439e35
SURAJ C S <surajcs@outlook.com>
2021-05-30 01:48:29 +0530

    impilimented with Date time Library

--- investDatadownloader.py
+++ investDatadownloader.py
@@ -1,6 +1,14 @@
 import investpy
+import datetime
+import pandas as pd
 
 
-df = investpy.get_stock_historical_data(stock='HIAE',country='india', from_date='01/01/2000', to_date='01/01/2021')
+stocks=["HIAE","INFY","HDFC"]
+
+start=datetime.date.today()-datetime.timedelta(10000)
+enddate=datetime.date.today()
+startString = start.strftime("%d/%m/%Y")
+endString = enddate.strftime("%d/%m/%Y")
+df = investpy.get_stock_historical_data(stock='INFY',country='india', from_date=startString, to_date=endString)
 print(df.head())                                    
-                        
+df.to_csv (r'C:\Users\suraj\Desktop\IFY.csv', index = True, header=True)                        
