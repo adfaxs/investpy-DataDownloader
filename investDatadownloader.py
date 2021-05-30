d40c1942c68a0652d712b8119350767568acc039
SURAJ C S <surajcs@outlook.com>
2021-05-30 11:06:53 +0530

    Implimented tickers sourced through external file

--- investDatadownloader.py
+++ investDatadownloader.py
@@ -2,14 +2,21 @@ import investpy
 import datetime
 import pandas as pd
 
-
-stocks=["HIAE","INFY","HDFC"]
-
 start=datetime.date.today()-datetime.timedelta(10000)
 enddate=datetime.date.today()
 startString = start.strftime("%d/%m/%Y")
 endString = enddate.strftime("%d/%m/%Y")
 
-for ticker in stocks:
-    df = investpy.get_stock_historical_data(stock=ticker,country='india', from_date=startString, to_date=endString)
-    df.to_csv(r'C:\Users\suraj\Desktop\%s.csv'%ticker)
+f=open("stocklist.txt","r")
+for ticker in f:
+     df = investpy.get_stock_historical_data(stock=ticker.strip(),country='india', from_date=startString, to_date=endString)
+     df.to_csv(r'C:\Users\suraj\Desktop\%s.csv'%ticker.strip())
+    
+
+
+
+
+
+
+
+
