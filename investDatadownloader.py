cfcfdcf08ee1c2090559c188278a17d16d3f770f
SURAJ C S <surajcs@outlook.com>
2021-05-30 13:48:46 +0530

    Progress bar added

--- investDatadownloader.py
+++ investDatadownloader.py
@@ -1,17 +1,33 @@
 import investpy
 import datetime
-import pandas as pd
+import os
+import progressbar
+from time import sleep
 
 start=datetime.date.today()-datetime.timedelta(10000)
 enddate=datetime.date.today()
 startString = start.strftime("%d/%m/%Y")
 endString = enddate.strftime("%d/%m/%Y")
+path='C:\\Users\\suraj\\Desktop\\EOD DATA'
+if not os.path.exists(path):
+    os.makedirs(path)
 
 f=open("stocklist.txt","r")
+num_lines = sum(1 for line in f)
+f.seek(0)
+bar = progressbar.ProgressBar(maxval=num_lines,widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+bar.start()
+i=0
 for ticker in f:
      df = investpy.get_stock_historical_data(stock=ticker.strip(),country='india', from_date=startString, to_date=endString)
-     df.to_csv(r'C:\Users\suraj\Desktop\%s.csv'%ticker.strip())
+     df.to_csv(path +'\\%s.csv'%ticker.strip())
+     i=i+1
+     bar.update(i)
     
+f.close()
+bar.finish()
+
+
 
 
 
