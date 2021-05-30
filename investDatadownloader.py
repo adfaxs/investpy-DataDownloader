36983b74105838e952ce107400a58b55aaf7755b
SURAJ C S <surajcs@outlook.com>
2021-05-30 10:42:11 +0530

    Multiple ticker download as .csv is implimented

--- investDatadownloader.py
+++ investDatadownloader.py
@@ -9,6 +9,7 @@ start=datetime.date.today()-datetime.timedelta(10000)
 enddate=datetime.date.today()
 startString = start.strftime("%d/%m/%Y")
 endString = enddate.strftime("%d/%m/%Y")
-df = investpy.get_stock_historical_data(stock='INFY',country='india', from_date=startString, to_date=endString)
-print(df.head())                                    
-df.to_csv (r'C:\Users\suraj\Desktop\IFY.csv', index = True, header=True)                        
+
+for ticker in stocks:
+    df = investpy.get_stock_historical_data(stock=ticker,country='india', from_date=startString, to_date=endString)
+    df.to_csv(r'C:\Users\suraj\Desktop\%s.csv'%ticker)
