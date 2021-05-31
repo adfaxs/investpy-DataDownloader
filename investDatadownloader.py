5b92e178a20a6cca2212b9ee217c997db8ecd386
SURAJ C S <surajcs@outlook.com>
2021-05-31 17:19:48 +0530

    Ticker with full name downloaded as .csv

--- investDatadownloader.py
+++ investDatadownloader.py
@@ -13,17 +13,19 @@ if not os.path.exists(path):
     os.makedirs(path)
 
 f=open("stocklist.txt","r")
-num_lines = sum(1 for line in f if line.rstrip())
-f.seek(0)
+ticker=f.readlines()
+num_lines=len(ticker)
+# num_lines = sum(1 for line in f if line.rstrip())
+#f.seek(0)
 bar = progressbar.ProgressBar(maxval=num_lines,widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
 bar.start()
-i=0
-for ticker in f :
-     df = investpy.get_index_historical_data(index=ticker.strip(), country='india', from_date=startString, to_date=endString)
-     #df = investpy.get_stock_historical_data(stock=ticker.strip(),country='india', from_date=startString, to_date=endString)
-     df.to_csv(path +'\\%s.csv'%ticker.strip())
-     i=i+1
-     bar.update(i)
+
+for x in range(num_lines):
+     ticker[x]=ticker[x].split(",")
+     #df = investpy.get_index_historical_data(index=ticker.strip(), country='india', from_date=startString, to_date=endString)
+     df = investpy.get_stock_historical_data(stock=ticker[x][1],country='india', from_date=startString, to_date=endString)
+     df.to_csv(path +'\\%s.csv'%ticker[x][0])
+     bar.update(x)
     
 f.close()
 bar.finish()
